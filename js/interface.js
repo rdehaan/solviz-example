cb3c29b3967201427a8a2a56487c122c73d97349
Ronald de Haan <me@ronalddehaan.eu>
2023-07-23 22:12:53 +0200

    Update example for new solviz interface

--- js/interface.js
+++ js/interface.js
@@ -1,25 +1,46 @@
 // This file is released under the MIT license.
 // See LICENSE.md.
 
+lit_to_atom = {};
+model_found = false;
+
 var log = "";
 var logElement = document.getElementById('log');
 
+function get_atom_from_lit(lit) {
+  if (lit > 0) {
+    atom = lit_to_atom[lit];
+  }
+  else {
+    atom = "-" + lit_to_atom[-lit];
+  }
+  return atom;
+}
+
 function interface_register_watch(lit, atom) {
+  lit_to_atom[lit] = atom;
   addToLog("Interface: registered watch " + atom + " (literal: " + lit + ")");
 }
 function interface_propagate(lit, atom) {
+  atom = get_atom_from_lit(lit);
   addToLog("Interface: propagate " + atom + " (literal: " + lit + ")");
 }
 function interface_undo(lit, atom) {
+  if (!need_to_update_graphics()) {
+    return;
+  }
+  atom = get_atom_from_lit(lit);
   addToLog("Interface: undo " + atom + " (literal: " + lit + ")");
 }
-function interface_decide() {
-  addToLog("Interface: decide");
+function interface_decide(lit) {
+  atom = get_atom_from_lit(lit);
+  addToLog("Interface: decide " + atom + " (literal: " + lit + ")");
 }
 function interface_check(model) {
   addToLog("Interface: check; model: " + model);
   updateOutput();
-  if (document.getElementById("pause-on-model").checked) {
+  model_found = true;
+  if (need_to_update_graphics() && document.getElementById("pause-on-model").checked) {
     do_pause();
   }
 }
@@ -50,22 +71,37 @@ function interface_finish() {
   }, speed_factor*1000);
 }
 function interface_wait_time_propagate() {
+  if (!need_to_update_graphics()) {
+    return 0;
+  }
   speed_factor = document.getElementById("speed").value;
   return speed_factor*1000;
 }
 function interface_wait_time_undo() {
+  if (!need_to_update_graphics()) {
+    return 0;
+  }
   speed_factor = document.getElementById("speed").value;
   return speed_factor*1000;
 }
 function interface_wait_time_check() {
+  if (!need_to_update_graphics()) {
+    return 0;
+  }
   speed_factor = document.getElementById("speed").value;
   return speed_factor*2000;
 }
 function interface_wait_time_on_model() {
+  if (!need_to_update_graphics()) {
+    return 0;
+  }
   speed_factor = document.getElementById("speed").value;
   return speed_factor*0;
 }
 function interface_wait_time_decide() {
+  if (!need_to_update_graphics()) {
+    return 0;
+  }
   speed_factor = document.getElementById("speed").value;
   return speed_factor*500;
 }
@@ -80,6 +116,14 @@ function do_resume() {
   document.getElementById("pause").disabled = false;
   document.getElementById("resume").disabled = true;
   Module.can_resume = true;
+}
+
+function need_to_update_graphics() {
+  var index = document.getElementById("mode").selectedIndex;
+  if (index == 0 && model_found == true) {
+    return false;
+  }
+  return true;
 }
 
 function clearLog() {
@@ -98,6 +142,7 @@ function updateLog() {
     // logElement.scrollTop = logElement.scrollHeight; // focus on bottom
   }
 }
+
 
 clearLog();
 addToLog("Ready...");
