4b7261488238045a56ccab8ea54f11e320e88d47
Ronald de Haan <me@ronalddehaan.eu>
2023-07-31 22:17:13 +0200

    Fix typo

--- js/clingo-module.js
+++ js/clingo-module.js
@@ -28,7 +28,7 @@ input.getSession().on('change', function() {
 var stored_input = localStorage.getItem("solviz-example-input");
 if (stored_input) {
   input.setValue(stored_input);
-  inputElement.execCommand("gotolineend");
+  input.execCommand("gotolineend");
 }
 
 function load_example() {
